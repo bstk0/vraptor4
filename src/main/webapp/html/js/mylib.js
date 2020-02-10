14279ef871d7c2e9472e978429170eb1a1c8da2a
rodrigo.bisterco <rodrigo.bisterco@gmail.com>
2020-02-10 15:09:55 -0300

    pagtos query string

---
+++ src/main/webapp/html/js/mylib.js
@@ -0,0 +1,18 @@
+/**
+ * mylib - 10.02 - v1 
+ */
+
+function GetURLParameter(sParam) {
+	console.log("GetURLParameter.sParam:" + sParam);
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+    //return "";
+}
