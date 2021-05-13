6b59bb52d7e84506026264c13702809414d0cf37
bstk0 <rodrigo.bisterco@gmail.com>
2021-05-13 15:21:15 -0300

    novo commit

--- src/main/java/br/com/caelum/vraptor/dbUtil/MongoDBmLab.java
+++ src/main/java/br/com/caelum/vraptor/dbUtil/MongoDBmLab.java
@@ -13,18 +13,17 @@ public class MongoDBmLab {
 	// mongodb://<dbuser>:<dbpassword>@ds013221.mlab.com:13221/dbe
 	// private final String URI = "mongodb://user1:usuario1@ds013221.mlab.com:13221/dbe";
 	//13.05.21
-
 	//private final String URI = "mongodb+srv://bisteka1:Bstk1234@my-atlas-cluster-mdyjt.mongodb.net/dbe?retryWrites=true&w=majority";
-	private final String URI = "mongodb://bisteka1:Bstk1234@my-atlas-cluster-shard-00-00-mdyjt.mongodb.net:27017,"+ 
-	"my-atlas-cluster-shard-00-01-mdyjt.mongodb.net:27017," +
-	"my-atlas-cluster-shard-00-02-mdyjt.mongodb.net:27017/dbe?ssl=true&replicaSet=my-atlas-cluster-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-	
 	@SuppressWarnings("resource")
 	private MongoDatabase getDbConnection() {
-		MongoClientURI uri  = new MongoClientURI(URI); 
-        MongoClient client = new MongoClient(uri);
-        return client.getDatabase(uri.getDatabase());
+		//MongoClientURI uri  = new MongoClientURI(URI); 
+        //MongoClient client = new MongoClient(uri);
+        //return client.getDatabase(uri.getDatabase());
+
+		MongoClient mongoClient = MongoClients.create(
+			"mongodb+srv://bisteka1:Bstk1234@cluster0.l0lec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+		return database = mongoClient.getDatabase("dbe");	
 	}
 	
 	public MongoCursor<Document> getCollection(String colletionName) {
