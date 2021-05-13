4eb06183e969e5140128986e4e0976406fea3eda
bstk0 <rodrigo.bisterco@gmail.com>
2021-05-13 15:41:31 -0300

    .

--- src/main/java/br/com/caelum/vraptor/dbUtil/MongoDBmLab.java
+++ src/main/java/br/com/caelum/vraptor/dbUtil/MongoDBmLab.java
@@ -2,8 +2,8 @@ package br.com.caelum.vraptor.dbUtil;
 
 import org.bson.Document;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
